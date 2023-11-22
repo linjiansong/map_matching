807160416ab5410723eef1fae74747c181c0e480
linjs <linjs@xiaopeng.com>
2023-11-22 16:13:26 +0800

    save road segment into dictionary

--- hhb.py
+++ hhb.py
@@ -4,9 +4,8 @@ import time
 import numpy as np
 from openpyxl import load_workbook
 from pykml import parser
-import requests
-from openpyxl import Workbook
-import multiprocessing
+import xmltodict
+import json
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "../.."))
@@ -18,7 +17,7 @@ PI = 3.1415926535897932384626  # π
 def ParseKmlData(kml_path):
     # 解析KML文件
     kml_data = None
-    anchor = []
+    unique_anchor = []
     with open(kml_path, 'r') as f:
         kml_data = parser.parse(f).getroot()
 
@@ -28,29 +27,37 @@ def ParseKmlData(kml_path):
     # 查找Placemark元素
     placemarks = kml_data.findall('.//kml:Placemark', namespace)
 
-    traj_enu_points = []
     count = 1
     print("len(placemarks) = {}".format(len(placemarks)))
+
+    road_segment_by_name = {}
     for placemark in placemarks:
+        # description_dict = xmltodict.parse(placemark.description.text.strip())
+        description_dict = json.loads(placemark.description.text.strip())
+        road_name = description_dict['Name']
+
+        road_enu_points = []
+        # 这里需要特别注意，不同的kml文件，LineString的坐标点的分隔符可能是空格，也可能是换行符
         for coordinate in placemark.LineString.coordinates.text.strip().split("\n"):
                 parts = coordinate.split(',')
                 longitude = float(parts[0])
                 latitude = float(parts[1])
-                # print(longitude, latitude)
-                count += 1
-                if len(anchor) == 0:
-                    anchor = np.array([longitude * PI / 180, latitude * PI / 180, 0])
+                if len(unique_anchor) == 0:
+                    unique_anchor = np.array([longitude * PI / 180, latitude * PI / 180, 0])
 
-                enu_point = lla2enu(longitude * PI / 180, latitude * PI / 180, 0, anchor)
-                traj_enu_points.append(enu_point)
+                enu_point = lla2enu(longitude * PI / 180, latitude * PI / 180, 0, unique_anchor)
+                road_enu_points.append(enu_point)
+        for segment_idx in range(len(road_enu_points) - 1):
+            road_segment_name = road_name + "_" + str(segment_idx)
+            road_segment_by_name[road_segment_name] = road_enu_points[segment_idx:segment_idx + 2]
+        
     print("count = {}".format(count))
-    return traj_enu_points, anchor
-
+    return road_segment_by_name, unique_anchor
 
 
 if __name__ == "__main__":
     kml_file = "./data/xian_road.kml"
     t1 = time.time()
-    ParseKmlData = ParseKmlData(kml_file)
+    road_segment_by_name, unique_anchor = ParseKmlData(kml_file)
     t2 = time.time()
     print("t2 - t1 = {}".format(t2 - t1))
