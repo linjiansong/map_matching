67ba4d4e6f35f68b7a76f5bf065f6c30b82f3503
linjs <linjs@xiaopeng.com>
2023-11-23 17:44:39 +0800

    parse user trajectory

--- hhb.py
+++ hhb.py
@@ -6,6 +6,7 @@ from openpyxl import load_workbook
 from pykml import parser
 import xmltodict
 import json
+import simplekml
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "../.."))
@@ -14,7 +15,65 @@ from util import lla2enu
 
 PI = 3.1415926535897932384626  # π
 
-def ParseKmlData(kml_path):
+class MapMatchingByHMM:
+    def __init__(self) -> None:
+        self.road_segment_by_name = None
+        self.road_segment_names = None
+        self.transform_probability = None
+
+    def SetRoadNetwork(self, road_segment_by_name):
+        self.road_segment_by_name = road_segment_by_name
+        self.road_segment_names = list(road_segment_by_name.keys())
+        self.transform_probability = self.GetTransformProbability()
+
+    def FindConnection(self, resolution):
+        road_segments = list(self.road_segment_by_name.items())
+        connection_info = {}
+        for road_name, road_segment in road_segments:
+            connection_info[road_name] = []
+
+        road_name_by_pixel = {}
+        for road_name, road_segment in road_segments:
+            start = road_segment[0]
+            end = road_segment[-1]
+            for point in [start, end]:
+                key = tuple((point[0:2] / resolution).astype(np.int32))
+                if key in road_name_by_pixel:
+                    road_name_by_pixel[key].append(road_name)
+                else:
+                    road_name_by_pixel[key] = [road_name]
+        return road_name_by_pixel
+
+    def GetTransformProbability(self):
+        resolution = 0.1
+        road_name_by_pixel = self.FindConnection(resolution)
+
+        # row is current state, column is next state
+        transform_probability = np.zeros((len(self.road_segment_names), len(self.road_segment_names)))
+        for road_idx, road_name in enumerate(self.road_segment_names):
+            road_segment = road_segment_by_name[road_name]
+            start_key = tuple((road_segment[0][0:2] / resolution).astype(np.int32))
+            end_key = tuple((road_segment[-1][0:2] / resolution).astype(np.int32))
+
+            neighbor_road_segments = set()
+            neighbor_road_segments.update(road_name_by_pixel[start_key])
+            neighbor_road_segments.update(road_name_by_pixel[end_key])
+
+            probability = 1.0 / len(neighbor_road_segments)
+
+            for road_segment_name in neighbor_road_segments:
+                neighbor_road_idx = self.road_segment_names.index(road_segment_name)
+                transform_probability[road_idx, neighbor_road_idx] = probability
+        return transform_probability
+    
+    def FindMatchedPath(self, traj_points):
+        if self.road_segment_by_name is None:
+            print("Error: road network is not set!")
+            return None
+        
+        return []
+
+def ParseRoadNetworkKmlData(kml_path):
     # 解析KML文件
     kml_data = None
     unique_anchor = []
@@ -56,59 +115,40 @@ def ParseKmlData(kml_path):
         
     return road_segment_by_name, unique_anchor
 
-def FindConnection(road_segment_by_name):
-    road_segments = list(road_segment_by_name.items())
-    connection_info = {}
-    for road_name, road_segment in road_segments:
-        connection_info[road_name] = []
+def ParseTrajectoryKmlData(kml_path, unique_anchor):
+    # 解析KML文件
+    kml_data = None
+    with open(kml_path, 'r') as f:
+        kml_data = parser.parse(f).getroot()
 
-    resolution = 0.1
-    road_name_by_pixel = {}
-    for road_name, road_segment in road_segments:
-        start = road_segment[0]
-        end = road_segment[-1]
-        for point in [start, end]:
-            key = tuple((point[0:2] / resolution).astype(np.int32))
-            if key in road_name_by_pixel:
-                road_name_by_pixel[key].append(road_name)
-            else:
-                road_name_by_pixel[key] = [road_name]
+    # 定义命名空间
+    namespace = {'kml': 'http://www.opengis.net/kml/2.2'}
 
-    return road_name_by_pixel
-    
-def GetTransformProbability(road_segment_by_name):
-    road_segment_names = list(road_segment_by_name.keys())
+    # 查找Placemark元素
+    placemarks = kml_data.findall('.//kml:Placemark', namespace)
 
-    road_name_by_pixel = FindConnection(road_segment_by_name)
-
-    resolution = 0.1
-    # row is current state, column is next state
-    transform_probability = np.zeros((len(road_segment_names), len(road_segment_names)))
-    for road_idx, road_name in enumerate(road_segment_names):
-        road_segment = road_segment_by_name[road_name]
-        start_key = tuple((road_segment[0][0:2] / resolution).astype(np.int32))
-        end_key = tuple((road_segment[-1][0:2] / resolution).astype(np.int32))
-
-        neighbor_road_segments = set()
-        neighbor_road_segments.update(road_name_by_pixel[start_key])
-        neighbor_road_segments.update(road_name_by_pixel[end_key])
-
-        probability = 1.0 / len(neighbor_road_segments)
-
-        for road_segment_name in neighbor_road_segments:
-            neighbor_road_idx = road_segment_names.index(road_segment_name)
-            transform_probability[road_idx, neighbor_road_idx] = probability
-        
-    return transform_probability
-
+    # 遍历Placemark元素
+    traj_points = []
+    for placemark in placemarks:
+        # 这里需要特别注意，不同的kml文件，LineString的坐标点的分隔符可能是空格，也可能是换行符
+        for coordinate in placemark.LineString.coordinates.text.strip().split(" "):
+                parts = coordinate.split(',')
+                longitude = float(parts[0])
+                latitude = float(parts[1])
+                enu_point = lla2enu(longitude * PI / 180, latitude * PI / 180, 0, unique_anchor)
+                traj_points.append(enu_point)
+    return traj_points, unique_anchor   
 
 if __name__ == "__main__":
-    kml_file = "./data/xian_road.kml"
+    road_kml_file = "./data/road_network/xian_road.kml"
     t1 = time.time()
-    road_segment_by_name, unique_anchor = ParseKmlData(kml_file)
-    connection_info = FindConnection(road_segment_by_name)
-    road_segment_names = list(road_segment_by_name.keys())
-    transform_probability = GetTransformProbability(road_segment_by_name)
+    road_segment_by_name, unique_anchor = ParseRoadNetworkKmlData(road_kml_file)
 
-    t2 = time.time()
-    print("t2 - t1 = {}".format(t2 - t1))
+    # map_matcher = MapMatchingByHMM()
+    # map_matcher.SetRoadNetwork(road_segment_by_name)
+    # t2 = time.time()
+    # print("take {}s to set road network".format(t2 - t1))
+
+    traj_kml_file = "./data/trajectories/ed40697bfde345984ac096bd656df62f.kml"
+    t1 = time.time()
+    traj_points = ParseTrajectoryKmlData(traj_kml_file, unique_anchor)
---
+++ parse_trajectories.py
@@ -0,0 +1,44 @@
+import simplekml
+
+def SaveTrajecoryAlKml(traj_points, file_path):
+    kml = simplekml.Kml()
+
+    # add line elements
+    line = kml.newlinestring(name="traj")
+    for point in traj_points:
+        line.coords.addcoordinates([(point[1], point[2], 0)])
+
+    line.style.linestyle.color = simplekml.Color.red
+    line.style.linestyle.width = 3
+    kml.save(file_path)
+
+def ParseTrajectories(traj_path):
+    # 打开文件
+    file = open(traj_path, "r")
+    trajectrory_by_order_id = {}
+    for line in file:
+        result = line.split(",")
+        user_id = result[0]
+        order_id = result[1]
+        time_stamp = int(result[2])
+        longitude = float(result[3])
+        latitude = float(result[4])
+        if order_id not in trajectrory_by_order_id:
+            trajectrory_by_order_id[order_id] = [(time_stamp, longitude, latitude)]
+        else:
+            trajectrory_by_order_id[order_id].append((time_stamp, longitude, latitude))
+    print("len(trajectrory_by_order_id) = {}".format(len(trajectrory_by_order_id)))
+
+    valid_traj_num = 0
+    for order_id, traj_points in trajectrory_by_order_id.items():
+        # 这里只保留轨迹点数大于1000的轨迹，实际上应该过滤，但是不过滤轨迹量太多了
+        if len(traj_points) < 1000:
+            continue
+        traj_points.sort(key=lambda x: x[0])
+        valid_traj_num += 1
+        SaveTrajecoryAlKml(traj_points, "./data/trajectories/{}.kml".format(order_id))
+    print("valid_traj_num = {}".format(valid_traj_num))
+
+if __name__ == "__main__":
+    traj_file = "./data/trajectories/gps_20161001.txt"
+    ParseTrajectories(traj_file)
